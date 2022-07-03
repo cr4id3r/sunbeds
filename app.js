19ceb14aab079890acbbc8166aafb471ea20bb7d
Kawasaki93 <108570453+Kawasaki93@users.noreply.github.com>
2022-07-03 22:14:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -185,9 +185,4 @@ function exportHistory() {
     console.log("finished");    
 }
 
-$('.no-zoom').bind('touchend', function(e) {
-  e.preventDefault();
-  // Add your code here. 
-  
-  // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
-})
+
