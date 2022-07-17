0a39238af1f1dbb2fd563bf2c70660a98ecf5c78
Adrian Cabeza <adrian@adrian-mac.local>
2022-07-17 12:21:51 +0200

    Configuring deployment..

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-import sys
+import sys, os
 
 from src.main import app
 
 if __name__ == '__main__':
-    sys.path.append('./src')
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './src')))
     app.run()
