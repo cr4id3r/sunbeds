be3ec10eac1e827a45b6f354252fa4d432484ac5
Adrian Cabeza <adrian@adrian-mac.local>
2022-07-17 12:27:41 +0200

    Configuring deployment..

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+web: gunicorn --chdir ./src wsgi:app
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-from src.main import app
+from main import app
 
 if __name__ == '__main__':
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './src')))
